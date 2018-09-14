c1a1823424fed94a32802e66f72cb9d1f43add50
Christopher Piggott <cpiggott@gmail.com>
2018-09-13 21:31:58 -0400

    Split thermostat states

---
+++ insteonplm/states/climateControl_th2441th.py
@@ -0,0 +1,183 @@
+from insteonplm.constants import COMMAND_THERMOSTAT_HUMIDITY_STATUS_0X6F_NONE, MESSAGE_TYPE_DIRECT_MESSAGE, \
+    COMMAND_EXTENDED_GET_SET_0X2E_0X00, ThermostatMode, COMMAND_THERMOSTAT_MODE_STATUS_0X70_NONE, \
+    COMMAND_THERMOSTAT_CONTROL_ON_HEAT_0X6B_0X04, MESSAGE_TYPE_DIRECT_MESSAGE_ACK, \
+    COMMAND_THERMOSTAT_CONTROL_ON_COOL_0X6B_0X05, COMMAND_THERMOSTAT_CONTROL_ON_AUTO_0X6B_0X06, \
+    COMMAND_THERMOSTAT_CONTROL_OFF_ALL_0X6B_0X09, COMMAND_THERMOSTAT_CONTROL_ON_FAN_0X6B_0X07, \
+    COMMAND_THERMOSTAT_CONTROL_OFF_FAN_0X6B_0X08, COMMAND_THERMOSTAT_COOL_SET_POINT_STATUS_0X71_NONE, \
+    COMMAND_THERMOSTAT_HEAT_SET_POINT_STATUS_0X72_NONE
+from insteonplm.messages import StandardReceive, ExtendedReceive
+from insteonplm.messages.messageFlags import MessageFlags
+from insteonplm.messages.userdata import Userdata
+from insteonplm.states import State
+from insteonplm.states.thermostat import Temperature, Humidity, SystemMode, FanMode, HeatSetPoint, CoolSetPoint
+
+
+class Temperature_2441th(Temperature):
+
+    def _register_messages(self):
+        humidity_msg = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_HUMIDITY_STATUS_0X6F_NONE,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
+
+        self._message_callbacks.add(humidity_msg, self._humidity_received)
+        ext_status_recd = ExtendedReceive.template(
+            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
+            cmd2=0x02,
+            userdata=Userdata.template({"d1": 0x01}))
+        self._message_callbacks.add(ext_status_recd,
+                                    self._ext_status_received)
+
+    def _ext_status_received(self, msg):
+        c_temp = msg.userdata['d10'] | (msg.userdata['d9'] << 8)
+        f_temp = c_temp * .1 * 9 / 5 + 32
+        self._update_subscribers(f_temp)
+
+class Humidity_2441th(Humidity):
+
+    def _ext_status_received(self, msg):
+        humid = msg.userdata['d8']
+        self._update_subscribers(humid)
+
+class SystemMode_2441th(SystemMode):
+
+    def _register_messages(self):
+        mode_status_msg = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_MODE_STATUS_0X70_NONE,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
+        mode_change_heat_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_HEAT_0X6B_0X04,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        mode_change_cool_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_COOL_0X6B_0X05,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        mode_change_auto_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_AUTO_0X6B_0X06,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        mode_change_off_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_OFF_ALL_0X6B_0X09,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        ext_status_recd = ExtendedReceive.template(
+            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
+            cmd2=0x02,
+            userdata=Userdata.template({"d1": 0x01}))
+
+        self._message_callbacks.add(mode_status_msg,
+                                    self._status_received)
+        self._message_callbacks.add(mode_change_heat_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(mode_change_cool_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(mode_change_auto_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(mode_change_off_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(ext_status_recd,
+                                    self._ext_status_received)
+
+class FanMode_2441th(FanMode):
+
+    def _register_messages(self):
+        mode_status_msg = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_MODE_STATUS_0X70_NONE,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
+        mode_change_fan_on_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_FAN_0X6B_0X07,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        mode_change_fan_auto_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_OFF_FAN_0X6B_0X08,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        mode_change_off_ack = StandardReceive.template(
+            commandtuple=COMMAND_THERMOSTAT_CONTROL_OFF_ALL_0X6B_0X09,
+            address=self._address,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
+        ext_status_recd = ExtendedReceive.template(
+            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
+            cmd2=0x02,
+            userdata=Userdata.template({"d1": 0x01}))
+
+        self._message_callbacks.add(mode_status_msg,
+                                    self._status_received)
+        self._message_callbacks.add(mode_change_fan_on_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(mode_change_fan_auto_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(mode_change_off_ack,
+                                    self._mode_change_ack)
+        self._message_callbacks.add(ext_status_recd,
+                                    self._ext_status_received)
+
+
+    def _ext_status_received(self, msg):
+        sysmode = msg.userdata['d6']
+        ext_mode = sysmode >> 4
+        if ext_mode == 0:
+            mode = ThermostatMode.OFF
+        elif ext_mode == 1:
+            mode = ThermostatMode.AUTO
+        elif ext_mode == 2:
+            mode = ThermostatMode.HEAT
+        elif ext_mode == 3:
+            mode = ThermostatMode.COOL
+        self._update_subscribers(mode)
+
+        sysmode = msg.userdata['d6']
+        ext_mode = sysmode & 0x0f
+        if ext_mode == 0:
+            mode = ThermostatMode.FAN_AUTO
+        elif ext_mode == 1:
+            mode = ThermostatMode.FAN_ALWAYS_ON
+        if mode:
+            self._update_subscribers(mode)
+
+
+class CoolSetPoint_2441th(CoolSetPoint):
+
+    def _register_messages(self):
+        cool_set_point_status = StandardReceive.template(
+            address=self._address,
+            commandtuple=COMMAND_THERMOSTAT_COOL_SET_POINT_STATUS_0X71_NONE,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, False))
+        self._message_callbacks.add(cool_set_point_status,
+                                    self._status_message_received)
+        ext_status_recd = ExtendedReceive.template(
+            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
+            cmd2=0x02,
+            userdata=Userdata.template({"d1": 0x01}))
+        self._message_callbacks.add(ext_status_recd,
+                                    self._ext_status_received)
+
+        def _ext_status_received(self, msg):
+            cool_sp = msg.userdata['d7'] / 2
+            self._update_subscribers(cool_sp)
+
+
+class HeatSetPoint_2441th(HeatSetPoint):
+
+    def _register_messages(self):
+        heat_set_point_status = StandardReceive.template(
+            address=self._address,
+            commandtuple=COMMAND_THERMOSTAT_HEAT_SET_POINT_STATUS_0X72_NONE,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, False))
+        self._message_callbacks.add(heat_set_point_status,
+                                    self._status_message_received)
+        ext_status_recd = ExtendedReceive.template(
+            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
+            cmd2=0x02,
+            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, True),
+            userdata=Userdata.template({"d1": 0x01}))
+
+        self._message_callbacks.add(ext_status_recd,
+                                    self._ext_status_received)
+
+    def _ext_status_received(self, msg):
+        heat_sp = msg.userdata['d12'] / 2
+        self._update_subscribers(heat_sp)
--- insteonplm/states/thermostat.py
+++ insteonplm/states/thermostat.py
@@ -1,5 +1,7 @@
 """Thermostat states."""
 import logging
+from abc import abstractmethod
+
 from insteonplm.constants import (
     COMMAND_EXTENDED_GET_SET_0X2E_0X00,
     # COMMAND_THERMOSTAT_TEMPERATURE_UP_0X68_NONE,
@@ -63,25 +65,13 @@ class Temperature(State):
         _LOGGER.debug("Received temperature value update")
         self._update_subscribers(msg.cmd2 / 2)
 
+    @abstractmethod
     def _register_messages(self):
-        temp_msg = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_TEMPERATURE_STATUS_0X6E_NONE,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
+        pass
 
-        self._message_callbacks.add(temp_msg, self._temp_received)
-        ext_status_recd = ExtendedReceive.template(
-            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
-            cmd2=0x02,
-            userdata=Userdata.template({"d1": 0x01}))
-        self._message_callbacks.add(ext_status_recd,
-                                    self._ext_status_received)
-
+    @abstractmethod
     def _ext_status_received(self, msg):
-        c_temp = msg.userdata['d10'] | (msg.userdata['d9'] << 8)
-        f_temp = c_temp * .1 * 9 / 5 + 32
-        self._update_subscribers(f_temp)
-
+        pass
 
 class Humidity(State):
     """A state representing a humidity sensor."""
@@ -112,24 +102,13 @@ class Humidity(State):
         _LOGGER.debug("Received humidity value")
         self._update_subscribers(msg.cmd2)
 
+    @abstractmethod
     def _register_messages(self):
-        humidity_msg = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_HUMIDITY_STATUS_0X6F_NONE,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
+        pass
 
-        self._message_callbacks.add(humidity_msg, self._humidity_received)
-        ext_status_recd = ExtendedReceive.template(
-            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
-            cmd2=0x02,
-            userdata=Userdata.template({"d1": 0x01}))
-        self._message_callbacks.add(ext_status_recd,
-                                    self._ext_status_received)
-
+    @abstractmethod
     def _ext_status_received(self, msg):
-        humid = msg.userdata['d8']
-        self._update_subscribers(humid)
-
+        pass
 
 class SystemMode(State):
     """A state representing the thermostat mode."""
@@ -196,58 +175,13 @@ class SystemMode(State):
         _LOGGER.info("mode standard status received")
         self._update_subscribers(ThermostatMode(msg.cmd2))
 
+    @abstractmethod
     def _ext_status_received(self, msg):
-        _LOGGER.info("mode extended status received")
-        sysmode = msg.userdata['d6']
-        ext_mode = sysmode >> 4
-        if ext_mode == 0:
-            mode = ThermostatMode.OFF
-        elif ext_mode == 1:
-            mode = ThermostatMode.AUTO
-        elif ext_mode == 2:
-            mode = ThermostatMode.HEAT
-        elif ext_mode == 3:
-            mode = ThermostatMode.COOL
-        self._update_subscribers(mode)
+        pass
 
+    @abstractmethod
     def _register_messages(self):
-        mode_status_msg = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_MODE_STATUS_0X70_NONE,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
-        mode_change_heat_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_HEAT_0X6B_0X04,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        mode_change_cool_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_COOL_0X6B_0X05,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        mode_change_auto_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_AUTO_0X6B_0X06,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        mode_change_off_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_OFF_ALL_0X6B_0X09,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        ext_status_recd = ExtendedReceive.template(
-            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
-            cmd2=0x02,
-            userdata=Userdata.template({"d1": 0x01}))
-
-        self._message_callbacks.add(mode_status_msg,
-                                    self._status_received)
-        self._message_callbacks.add(mode_change_heat_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(mode_change_cool_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(mode_change_auto_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(mode_change_off_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(ext_status_recd,
-                                    self._ext_status_received)
+        pass
 
 
 class FanMode(State):
@@ -310,49 +244,14 @@ class FanMode(State):
         _LOGGER.debug("Fan standard status message received")
         self._update_subscribers(ThermostatMode(msg.cmd2))
 
+    @abstractmethod
     def _ext_status_received(self, msg):
-        _LOGGER.debug("Fan extended status message received")
-        sysmode = msg.userdata['d6']
-        ext_mode = sysmode & 0x0f
-        if ext_mode == 0:
-            mode = ThermostatMode.FAN_AUTO
-        elif ext_mode == 1:
-            mode = ThermostatMode.FAN_ALWAYS_ON
-        if mode:
-            self._update_subscribers(mode)
+        pass
 
+    @abstractmethod
     def _register_messages(self):
-        mode_status_msg = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_MODE_STATUS_0X70_NONE,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, None))
-        mode_change_fan_on_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_ON_FAN_0X6B_0X07,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        mode_change_fan_auto_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_OFF_FAN_0X6B_0X08,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        mode_change_off_ack = StandardReceive.template(
-            commandtuple=COMMAND_THERMOSTAT_CONTROL_OFF_ALL_0X6B_0X09,
-            address=self._address,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE_ACK))
-        ext_status_recd = ExtendedReceive.template(
-            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
-            cmd2=0x02,
-            userdata=Userdata.template({"d1": 0x01}))
+        pass
 
-        self._message_callbacks.add(mode_status_msg,
-                                    self._status_received)
-        self._message_callbacks.add(mode_change_fan_on_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(mode_change_fan_auto_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(mode_change_off_ack,
-                                    self._mode_change_ack)
-        self._message_callbacks.add(ext_status_recd,
-                                    self._ext_status_received)
 
 
 class CoolSetPoint(State):
@@ -394,24 +293,13 @@ class CoolSetPoint(State):
         _LOGGER.debug("Cooling standard status received")
         self._update_subscribers(msg.cmd2 / 2)
 
+    @abstractmethod
     def _register_messages(self):
-        cool_set_point_status = StandardReceive.template(
-            address=self._address,
-            commandtuple=COMMAND_THERMOSTAT_COOL_SET_POINT_STATUS_0X71_NONE,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, False))
-        self._message_callbacks.add(cool_set_point_status,
-                                    self._status_message_received)
-        ext_status_recd = ExtendedReceive.template(
-            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
-            cmd2=0x02,
-            userdata=Userdata.template({"d1": 0x01}))
-        self._message_callbacks.add(ext_status_recd,
-                                    self._ext_status_received)
+        pass
 
+    @abstractmethod
     def _ext_status_received(self, msg):
-        _LOGGER.debug("Cooling extended status received")
-        cool_sp = msg.userdata['d7'] / 2
-        self._update_subscribers(cool_sp)
+        pass
 
 
 class HeatSetPoint(State):
@@ -452,24 +340,10 @@ class HeatSetPoint(State):
         _LOGGER.debug("Heating standard status received")
         self._update_subscribers(msg.cmd2 / 2)
 
+    @abstractmethod
     def _register_messages(self):
-        _LOGGER.debug('Starting HeatSetPoint register_messages')
-        heat_set_point_status = StandardReceive.template(
-            address=self._address,
-            commandtuple=COMMAND_THERMOSTAT_HEAT_SET_POINT_STATUS_0X72_NONE,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, False))
-        self._message_callbacks.add(heat_set_point_status,
-                                    self._status_message_received)
-        ext_status_recd = ExtendedReceive.template(
-            commandtuple=COMMAND_EXTENDED_GET_SET_0X2E_0X00,
-            cmd2=0x02,
-            flags=MessageFlags.template(MESSAGE_TYPE_DIRECT_MESSAGE, True),
-            userdata=Userdata.template({"d1": 0x01}))
-        _LOGGER.debug('Reg Ext Status: %s', ext_status_recd)
-        self._message_callbacks.add(ext_status_recd,
-                                    self._ext_status_received)
+        pass
 
+    @abstractmethod
     def _ext_status_received(self, msg):
-        _LOGGER.debug("Heating extended status received")
-        heat_sp = msg.userdata['d12'] / 2
-        self._update_subscribers(heat_sp)
+        pass
